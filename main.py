3eb81c1a9830ceb92744d8abd161f96e72aa4773
Nero15739 <andrewpajva@gmail.com>
2025-03-15 22:49:08 +1100

    Feat: Starter Program

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+# Algorithm Comparison Software where two seperate algorithms can be selected and face off against each other with varing inputs, both generated and non
+
+def main ():
+    print("Welcome to Compare Algos") 
+
+main()
